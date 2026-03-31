8bc39a34bf67ef99c3e4b8871bdac92a0cff411f
Ruch260920 <156458392+Ruch260920@users.noreply.github.com>
2026-03-31 11:43:59 +0200

    utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,9 @@
-# utils.py
 import requests
-import streamlit as st
 
-@st.cache_data
-def load_lottie_url(url: str):
-    """Loads Lottie animation from a URL"""
-    r = requests.get(url)
-    if r.status_code != 200:
+def load_lottie_url(url):
+    try:
+        r = requests.get(url)
+        if r.status_code == 200:
+            return r.json()
+    except:
         return None
-    return r.json()
